fd2460dc1a2669c1bcb03e4d5ae1f307fdf754c9
Pablinho100 <131218147+Pablinho100@users.noreply.github.com>
2026-04-10 18:38:36 -0300

    Crear Problema04B.cs

---
+++ Problema04/A/Problema04B.cs
@@ -0,0 +1,19 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- BLOQUE 3: TEXTO Y CARACTERES ---");
+
+            string nombreCompleto = "Pablo Alejandro Arancibia";
+            Console.WriteLine("4. TIPO TEXTO (string):");
+            Console.WriteLine("   Valor guardado: " + nombreCompleto);
+
+            char inicialNombre = 'P';
+            Console.WriteLine("\n5. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + inicialNombre + "'");
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
