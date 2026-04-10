894e385ecd7314ec1fe4824dbb7edd8cd1603fd1
Pablinho100 <131218147+Pablinho100@users.noreply.github.com>
2026-04-10 18:46:20 -0300

    Crear Problema02B.cs

---
+++ Problema02/A/Problema02B.cs
@@ -0,0 +1,20 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("--- BLOQUE 1: NÚMEROS ENTEROS ---");
+
+            int edad = 27;
+            int legajo = 64000;
+
+            Console.WriteLine("1. TIPO ENTERO (int):");
+            Console.WriteLine("   Para números sin decimales, como edad o cantidad.");
+            Console.WriteLine("   Valor guardado: " + edad);
+            Console.WriteLine("   Valor guardado: " + legajo);
+            
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
