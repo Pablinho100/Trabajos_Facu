c82f6e9bb974e96cb6b1ee3bf9ef6995fa83411e
Pablinho100 <131218147+Pablinho100@users.noreply.github.com>
2026-04-10 18:34:27 -0300

    Crear Problema00D.cs

---
+++ Problema00/A/Problema00D.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, mi nombre es Pablo Arancibia");
+        Console.WriteLine("Soy estudiante de TUP en la FRT UTN");
+        Console.WriteLine("Actualmente curso 2do año");
+        Console.WriteLine("Soy de SMT");
+        Console.WriteLine("Y tambien hincha de Boquita! :D ");
+    }
+}
